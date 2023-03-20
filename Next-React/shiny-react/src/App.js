35f567161dd765712f2a3dfbf88931c92d91c311
Alexandra-Georgina.Turtulea@adidas.com <altu@runtastic.com>
2023-03-20 23:19:01 +0100

    install i18n

--- Next-React/shiny-react/src/App.js
+++ Next-React/shiny-react/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ToDo from './components/ToDo';
 
+
 function App() {
   return (
     <>
+    
     <div className="todo-modal">
-      <h1>Here's my to-do!</h1>
+      <h2>Here's my to-do!</h2>
       <ToDo text="Get a job!"></ToDo>
       <div className="separator"></div>
       <ToDo text="Go to interview on Wednesday!"></ToDo>
