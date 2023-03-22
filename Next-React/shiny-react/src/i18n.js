6d2c40266f4db8bb4a136c2019230f87465ec48f
Alexandra-Georgina.Turtulea@adidas.com <altu@runtastic.com>
2023-03-22 23:04:30 +0100

    still need to fix i18n

---
+++ Next-React/shiny-react/src/i18n.js
@@ -0,0 +1,26 @@
+import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
+import translationEN from './locales/en/translation.json';
+import translationFR from './locales/fr/translation.json';
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  fr: {
+    translation: translationFR,
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
