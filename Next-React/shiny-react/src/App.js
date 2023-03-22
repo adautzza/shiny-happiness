7983927139466ee870bcb0107c8aa46a1fea50ad
Alexandra-Georgina.Turtulea@adidas.com <altu@runtastic.com>
2023-03-22 22:49:53 +0100

    add translations in fr and en

--- Next-React/shiny-react/src/App.js
+++ Next-React/shiny-react/src/App.js
@@ -1,29 +1,10 @@
-// eslint-disable-next-line 
-import { Trans,useTranslation } from 'react-i18next';
 import './App.css';
 import ToDo from './components/ToDo';
 
-const lngs = {
-  en: { nativeName: 'English' },
-  de: { nativeName: 'Deutsch' }
-};
-
 function App() {
-  const { t, i18n } = useTranslation();
 
   return (
     <>
-    <div>
-      {Object.keys(lngs).map((lng) => (
-      <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
-      {lngs[lng].nativeName}
-      </button>
-      ))}
-      </div>
-      <Trans i18nKey="description.part1">
-            Edit <code>src/App.js</code> and save to reload.
-          </Trans>
-
     <div className="todo-modal">
       <h2>Here's my to-do!</h2>
       <ToDo text="Get a job!"></ToDo>
