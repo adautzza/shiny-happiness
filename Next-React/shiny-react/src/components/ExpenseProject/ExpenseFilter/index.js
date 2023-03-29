37c9a8387bab26e937954c637d36af96da8e5c6b
Alexandra-Georgina.Turtulea@adidas.com <altu@runtastic.com>
2023-03-29 18:06:21 +0200

    enable filtering functionality for the pre-defined year, add user information when no data is found and enable select all option, set 2020 as default state

--- Next-React/shiny-react/src/components/ExpenseProject/ExpenseFilter/index.js
+++ Next-React/shiny-react/src/components/ExpenseProject/ExpenseFilter/index.js
@@ -1,19 +1,25 @@
 import "./index.css";
 
-const ExpensesFilter = () => {
+const ExpenseFilter = (props) => {
+  const expenseFilterHandler = (event) => {
+    props.onSelectExpenseYear(event.target.value);
+  };
+
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select value={props.selectedYear} onChange={expenseFilterHandler}>
+          <option value="2023">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
           <option value="2019">2019</option>
+          <option value="All">All</option>
         </select>
       </div>
     </div>
   );
 };
 
-export default ExpensesFilter;
+export default ExpenseFilter;
