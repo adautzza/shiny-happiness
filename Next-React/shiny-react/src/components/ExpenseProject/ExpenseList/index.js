b275f415ed23179f753444a7fb4f2347e1144cdc
Alexandra-Georgina.Turtulea@adidas.com <altu@runtastic.com>
2023-03-29 14:40:29 +0200

    remove filter which seemed to create a bug

--- Next-React/shiny-react/src/components/ExpenseProject/ExpenseList/index.js
+++ Next-React/shiny-react/src/components/ExpenseProject/ExpenseList/index.js
@@ -4,14 +4,11 @@ import ExpenseItem from "../ExpenseItem";
 
 const ExpenseList = (props) => {
   return (
-    <>
-      <ExpenseItem />
-      <Card className="expenses">
-        {props.expenses.map((expense) => (
-          <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id}></ExpenseItem>
-        ))}
-      </Card>
-    </>
+    <Card className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id}></ExpenseItem>
+      ))}
+    </Card>
   );
 };
 
