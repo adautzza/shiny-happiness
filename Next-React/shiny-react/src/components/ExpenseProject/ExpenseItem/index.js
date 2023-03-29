b7632d867c6da07b032523bb6e9f494bca0754fa
Alexandra-Georgina.Turtulea@adidas.com <altu@runtastic.com>
2023-03-29 18:31:19 +0200

    enhance font size and color for filtering

--- Next-React/shiny-react/src/components/ExpenseProject/ExpenseItem/index.js
+++ Next-React/shiny-react/src/components/ExpenseProject/ExpenseItem/index.js
@@ -1,21 +1,15 @@
-import { useState } from "react";
 import "./index.css";
 import Card from "../../generic/Card";
 import ExpenseDate from "../ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => setTitle("title");
-
   return (
     <>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
-          <button onClick={clickHandler}>Change!</button>
         </div>
       </Card>
     </>
