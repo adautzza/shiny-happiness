5d1b4ec0d6b5e395320dbd869a1d92e75f5df27c
Alexandra-Georgina.Turtulea@adidas.com <altu@runtastic.com>
2023-06-20 09:59:58 +0200

    update postService to gather all the necessary data

--- 2.Programming-Tasks/vanillajs-canonical/services/postService.js
+++ 2.Programming-Tasks/vanillajs-canonical/services/postService.js
@@ -1,18 +1,37 @@
-// Map the data from the API to the Post model
-
 const wordpressApi = require('../api/wordpress');
 const Post = require('../models/post');
 
 async function getPosts() {
-  const postsData = await wordpressApi.getPosts();
-  const posts = postsData.map(mapPostDataToModel);
-  return posts
+  try {
+    const postsData = await wordpressApi.getPosts();
+    const posts = postsData.map(mapPostDataToModel);
+    console.log(posts);
+    return posts;
+  } catch (error) {
+    console.error('Error occurred while fetching posts:', error);
+    return [];
+  }
 }
 
 function mapPostDataToModel(postData) {
-  const { id, date, title, link } = postData;
-  return new Post(id, date, title.rendered, link);
+  const { id, date, title, link, featured_media, _embedded } = postData;
+  const formattedDate = formatDate(date);
+  const author = _embedded && _embedded.author && _embedded.author[0] ? _embedded.author[0].name : 'Unknown author';
+  const category = _embedded && _embedded['wp:term'] && _embedded['wp:term'][2] && _embedded['wp:term'][2][0] ? _embedded['wp:term'][2][0].name : 'Uncategorized';
+
+  return new Post(id, formattedDate, title && title.rendered ? title.rendered : 'Untitled', link, featured_media, author, category);
 }
+
+function formatDate(dateString) {
+  const dateObject = new Date(dateString);
+  const day = dateObject.getDate();
+  const month = dateObject.toLocaleString('default', { month: 'long' });
+  const year = dateObject.getFullYear();
+  return `${day} ${month} ${year}`;
+}
+
+
+getPosts();
 
 module.exports = {
   getPosts,
