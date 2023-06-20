ed7ca803b68ce1ad4db6abe4384114f0fe426813
Alexandra-Georgina.Turtulea@adidas.com <altu@runtastic.com>
2023-06-20 10:46:52 +0200

    add template using ejs

--- 2.Programming-Tasks/vanillajs-canonical/index.js
+++ 2.Programming-Tasks/vanillajs-canonical/index.js
@@ -1,61 +1,23 @@
 const express = require('express');
 const postService = require('./services/postService');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
-const port = 3000; // You can change the port number if desired
+const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
   try {
     const posts = await postService.getPosts();
-    const html = generatePostHTML(posts);
-    res.send(html);
+    res.render('index', { posts });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).send('An error occurred');
   }
 });
-
-function generatePostHTML(posts) {
-  let html = `<!DOCTYPE html>
-    <html>
-    <head>
-      <link rel="stylesheet" href="https://assets.ubuntu.com/v1/vanilla-framework-version-2.10.1.min.css">
-      <style>
-        .card {
-          margin: 10px;
-          padding: 10px;
-          border: 1px solid #ccc;
-          border-radius: 4px;
-          width: 300px;
-        }
-      </style>
-    </head>
-    <body>`;
-
-  posts.forEach((post) => {
-    const card = `
-      <div class="card">
-        <div class="vf-card__title">${post.category}</div>
-        <div class="vf-card__image">
-          <img src="${post.featured_media}" alt="Blog Post Image">
-        </div>
-        <div class="vf-card__content">
-          <h3 class="vf-card__content-heading">${post.title}</h3>
-          <p class="vf-card__content-subheading">By ${post.author_name} on ${post.date}</p>
-          <p class="vf-card__content-type">${post.type}</p>
-        </div>
-      </div>
-    `;
-    html += card;
-  });
-
-  html += `
-    <script src="https://assets.ubuntu.com/v1/vanilla-framework-version-2.10.1.min.js"></script>
-    </body>
-    </html>`;
-
-  return html;
-}
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
